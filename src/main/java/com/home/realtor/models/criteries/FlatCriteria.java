0a7d45666ed01f0f9efb9b86117699d32ffc6a26
dandrusiv <dandrusiv@homeaway.com>
2020-01-02 20:10:02 +0200

    add the getters for criteria

--- src/main/java/com/home/realtor/models/criteries/FlatCriteria.java
+++ src/main/java/com/home/realtor/models/criteries/FlatCriteria.java
@@ -4,11 +4,14 @@ import com.home.realtor.models.Region;
 import com.home.realtor.models.enums.*;
 import lombok.Data;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Data
 public class FlatCriteria {
     int price;
+    boolean active;
     List<State> stateList;
     List<Heating> heatingList;
     List<HotWater> hotWaterList;
@@ -16,4 +19,41 @@ public class FlatCriteria {
     List<TypeBuilding> typeBuildingList;
     List<TypeFurniture> typeFurnitureList;
     List<Region> regionList;
+
+    public List<State> getStateList() {
+        return Objects.isNull(this.stateList)
+            ? new ArrayList<>()
+            : this.stateList;
+    }
+
+    public List<Heating> getHeatingList() {
+        return Objects.isNull(this.heatingList)
+            ? new ArrayList<>()
+            : this.heatingList;
+    }
+
+    public List<HotWater> getHotWaterList() {
+        return Objects.isNull(this.hotWaterList)
+            ? new ArrayList<>()
+            : this.hotWaterList;
+    }
+
+    public List<TypeRooms> getTypeRoomsList() {
+        return Objects.isNull(this.typeRoomsList)
+            ? new ArrayList<>()
+            : this.typeRoomsList;
+    }
+
+    public List<TypeFurniture> getTypeFurnitureList() {
+        return Objects.isNull(this.typeFurnitureList)
+            ? new ArrayList<>()
+            : this.typeFurnitureList;
+    }
+
+    public List<Region> getRegionList() {
+        return Objects.isNull(this.regionList)
+            ? new ArrayList<>()
+            : this.regionList;
+    }
+
 }
